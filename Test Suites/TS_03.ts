3076d2d91a8f6ea9d52092bd75b8673da94c310f
quynv36 <quynv36@PQA-QuyNV36.fishcm.com.vn>
2023-05-25 11:11:46 +0700

    Xong xong

---
+++ Test Suites/TS_03.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_03</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e1104f53-11fb-422f-96f9-983ef156f2f8</testSuiteGuid>
+</TestSuiteEntity>
