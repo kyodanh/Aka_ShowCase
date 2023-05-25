0f943876f7da84a7217445f529f35116711c8b8f
quynv36 <quynv36@PQA-QuyNV36.fishcm.com.vn>
2023-05-25 13:46:49 +0700

    j

--- Test Suites/TS_03.ts
+++ Test Suites/TS_03.ts
@@ -5,10 +5,24 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>e1104f53-11fb-422f-96f9-983ef156f2f8</testSuiteGuid>
+   <testCaseLink>
+      <guid>91e659c6-c2e8-4127-82d1-2bc517d2edd2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCase_Luong_DanhNC14/ThemMoi_BookMark</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b2edc908-eb88-4d67-b4e9-63350ff19310</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCase_Luong_DanhNC14/TimKiemVaXoaBookmarks</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
